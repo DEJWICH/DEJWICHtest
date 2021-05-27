382de457d63dc0b0f0f0ce4bf3f2cf831c8bdd00
DEJWICH <72445192+DEJWICH@users.noreply.github.com>
2021-05-27 12:04:11 +0200

    Add files via upload

---
+++ bepis.sh
@@ -0,0 +1,14 @@
+clear
+echo YOUR COMPUTER WILL DESTROY ITSELF IN 5 SECONDS
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
+echo 3
+sleep 1
+echo 2
+sleep 1
+echo 1
+sleep 1
+echo fuck off you bastard... 
